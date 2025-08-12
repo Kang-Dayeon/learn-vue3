34ff22612812f6780dc48255d125a6dafc7b47e6
kang-dayeon <dksldiss89@gmail.com>
2025-08-12 17:04:34 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+// import AppCard from './components/AppCard.vue'
+
+const app = createApp(App)
+
+// app.component('AppCard', AppCard)
+
+app.mount('#app')
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
